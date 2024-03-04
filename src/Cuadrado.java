f53921af658092991c3f9ab3567950d24c974ea0
Prog- <elizardo.powell@uip.edu.pa>
2024-03-04 15:40:38 -0500

    Calculadora

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,6 +1,7 @@
 public class Cuadrado {
     private float base, altura;
-    public void Cuadrado (float base, float altura){
+
+    public Cuadrado(float base, float altura) {
         this.altura = altura;
         this.base = base;
 
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    }
-    class Cuadrado{
-        private float base, altura;
-
-    }
-    public Cuadrado(base,altura){
+        Cuadrado base = new Cuadrado(10,15);
 
     }
 }
